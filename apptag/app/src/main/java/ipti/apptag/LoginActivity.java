3c8ab82bae41172e93b1008d4ebc846f1d145436
Adriano Dias <adriano.dias@ipti.info>
2016-03-04 11:44:44 -0300

    Commit inicial

---
+++ apptag/app/src/main/java/ipti/apptag/LoginActivity.java
@@ -0,0 +1,19 @@
+package ipti.apptag;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by AdrianoDias on 04/03/2016.
+ */
+public class LoginActivity extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.user_activity_login);
+//        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+//        setSupportActionBar(myToolbar);
+    }
+}
