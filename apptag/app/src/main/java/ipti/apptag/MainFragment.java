570f513c1f675ff0cddebee3d54555b09e969278
Adriano Dias <adriano.dias@ipti.info>
2016-03-11 11:34:53 -0300

    Tela de funcionalidades. (Menu lateral)

--- apptag/app/src/main/java/ipti/apptag/MainFragment.java
+++ apptag/app/src/main/java/ipti/apptag/MainFragment.java
@@ -1,10 +1,22 @@
 package ipti.apptag;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by AdrianoDias on 10/03/2016.
  */
 public class MainFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.content_fragment,container,false);
+        return v;
+    }
 }
+
